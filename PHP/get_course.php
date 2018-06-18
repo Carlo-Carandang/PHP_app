679faee6eb9b3031b2f83ebca11e3c5027de1937
Carlo Carandang <30868659+Carlo-Carandang@users.noreply.github.com>
2018-06-18 17:56:18 -0300

    Add files

---
+++ PHP/get_course.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+<title>
+get_course.php
+</title>
+</head>
+
+<body>
+
+<form action="insert_course.php" method="POST">
+<b>Enter the course name:</b>
+<input type="text" name="cname">
+<p>
+<b>Enter the course number:</b>
+<input type="text" name="cnum">
+<p>
+<b>Enter the number of credit hours:</b>
+<input type="text" name="chours">
+<p>
+<b>Enter the department code:</b>
+<input type="text" name="cdept">
+<p>
+<input type="submit" value="submit">
+</form>
+</body>
+
+<a href="main.php"> back to MAIN menu </a>
+
+</html>
